0a9c30035fe876f5b7f6771ea79bed43b28c8811
PedroGonçalvesBrito <pedrogoncalvesbrito0@gmail.com>
2026-01-26 18:45:47 -0400

    aloha

--- app/database/migrations/20260124011428_contact.php
+++ app/database/migrations/20260124011428_contact.php
@@ -6,19 +6,15 @@ use Phinx\Migration\AbstractMigration;
 
 final class Contact extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * Write your reversible migrations using this method.
-     *
-     * More information on writing migrations is available here:
-     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
-     *
-     * Remember to call "create()" or "update()" and NOT "save()" when working
-     * with the Table class.
-     */
+    
     public function change(): void
     {
-
+        $table = $this->table('contact', ['id' => false, 'primary_key' => ['id']]);
+        $table->addColumn('id', 'biginteger', ['identity' => true, 'null' => false])
+            ->addColumn('id_usuario', 'biginteger', ['null' => true])
+            ->addColumn('tipo', 'text', ['null' => true])
+            ->addColumn('endereco_contato', 'text', ['null' => true])
+            ->addForeignKey('id_usuario', 'users', 'id', ['delete' => 'CASCADE', 'update' => 'NO ACTION'])
+            ->create();
     }
 }
--- app/database/migrations/20260124014238_address.php
+++ app/database/migrations/20260124014238_address.php
@@ -6,19 +6,20 @@ use Phinx\Migration\AbstractMigration;
 
 final class Address extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * Write your reversible migrations using this method.
-     *
-     * More information on writing migrations is available here:
-     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
-     *
-     * Remember to call "create()" or "update()" and NOT "save()" when working
-     * with the Table class.
-     */
+
     public function change(): void
     {
-
+         $table = $this->table('address', ['id' => false, 'primary_key' => ['id']]);
+        $table->addColumn('id', 'biginteger', ['identity' => true, 'null' => false])
+            ->addColumn('id_usuario', 'biginteger', ['null' => true])
+            ->addColumn('titulo', 'text', ['null' => true])
+            ->addColumn('cep', 'text', ['null' => true])
+            ->addColumn('numero', 'text', ['null' => true])
+            ->addColumn('bairro', 'text', ['null' => true])
+            ->addColumn('cidade', 'text', ['null' => true])
+            ->addColumn('uf', 'text', ['null' => true])
+            ->addColumn('ibge', 'text', ['null' => true])
+            ->addForeignKey('id_usuario', 'users', 'id', ['delete' => 'CASCADE', 'update' => 'NO ACTION'])
+            ->create();
     }
 }
